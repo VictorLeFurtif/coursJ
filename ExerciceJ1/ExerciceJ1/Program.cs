94e3722ce2ee7dad77f6062ef62a3efaa7012638
Victor <victor.baz92@gmail.com>
2024-10-07 13:01:14 +0200

    Grosse avancer

--- ExerciceJ1/ExerciceJ1/Program.cs
+++ ExerciceJ1/ExerciceJ1/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace ExerciceJ1
+﻿using System;
+
+namespace ExerciceJ1
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            algo1 algo = new algo1();
+           int value = algo.Create_Value();
+            algo.SolveExercice(value);
         }
     }
 }
--- ExerciceJ1/ExerciceJ1/algo1.cs
+++ ExerciceJ1/ExerciceJ1/algo1.cs
@@ -1,7 +1,56 @@
+using System;
+using System.Runtime.Remoting.Messaging;
+
 namespace ExerciceJ1
 {
     public class algo1
     {
+        public int Create_Value()
+        {
+            Console.Write("Veuillez entrer une valeur entre 6 et 874 : ");
+            int value = int.Parse(Console.ReadLine());
+            return value;
+        }
+
+        public void SolveExercice(int value)
+        {
+            if (value > 6 || value < 874)
+            {
+                for (int i = 6; i < value; i++)
+                {
+                    if (CheckMultiple(i))
+                    {
+                        Console.Write(i + " ");
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+
+        public bool CheckMultiple(int i)
+        {
+            bool hasMultiple = false;
+            
+            if (i % 3 == 0 || i % 4 == 0)
+            {   
+                Console.Write("C ");
+                hasMultiple = true;
+            }
+
+            if (i % 7 == 0 || i % 18 == 0)
+            {
+                Console.Write("A ");
+                hasMultiple = true;
+            }
+
+            if (i % 42 == 0)
+            {
+                Console.Write("CA ");
+                hasMultiple = true;            }
+
+            return hasMultiple;
+        }
+        
         
     }
 }
